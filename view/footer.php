ef8b9dd0524259c2d6c4f6e544f8509117095376
taoshihan1991 <630892807@qq.com>
2020-11-27 11:14:07 +0800

    增加客服

--- view/footer.php
+++ view/footer.php
@@ -14,6 +14,15 @@ return <<<EOF
     <p>最简路由框架 <a href="https://github.com/taoshihan1991/simpleloader" target="_blank">SimpleLoader</a>，power by <a href="https://www.cnblogs.com/taoshihan" target="_blank">陶士涵的菜地</a></p>
   </div>
 </footer>
+
+<script>
+    var GOFLY_URL="https://gofly.sopans.com";
+    var GOFLY_KEFU_ID="kefu2";
+    var GOFLY_BTN_TEXT="与我交流 Chat with me";
+</script>
+<script src="https://gofly.sopans.com/static/js/gofly-front.js"></script>
+
+
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
